e01c9576b9896337b462ce7d9ec2dc8ba8a875d5
KrishnaYamphu <krishnayamphu@gmail.com>
2026-01-13 13:29:29 +0545

    js

---
+++ javascript/off-canvas.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const canvas = document.getElementById("myCanvas");
+  canvas.classList.toggle("move-canvas");
+}
+
+function closeMenu() {
+  const canvas = document.getElementById("myCanvas");
+  canvas.classList.remove("move-canvas");
+}
